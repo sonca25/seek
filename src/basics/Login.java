30fce6be24e78b1986d116028d7448ced837b8ec
sonca25 <sonca25@gmail.com>
2017-02-20 19:51:19 +1300

    clean up

--- src/basics/Login.java
+++ src/basics/Login.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class Login {
 	public static void main(String[] args){
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Alexandra\\Downloads\\chromedriver_win32\\chromedriver.exe");
